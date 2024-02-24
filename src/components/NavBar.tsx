8ac967a62e8b5a430e4fddf45d2ab1423022591c
Swaralee Panpatil <panpatilswaralee@gmail.comm>
2024-02-25 00:35:24 +0530

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { Stack, HStack, VStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text> Navbar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
